65f60236ce10bdf6f7ab705d5b633918b9519cb1
SUPAKORNEMCH\ponlo <supakorn.emch@gmail.com>
2023-05-21 18:45:14 +0700

    ,

--- linux/laravel/deploy.sh
+++ linux/laravel/deploy.sh
@@ -1,16 +1,11 @@
-# Copy config file
-cp /var/wwwroot/%APP_NAME%/deployments/site.prod.conf /etc/nginx/sites-available/default
-cp /var/wwwroot/%APP_NAME%/deployments/nginx.prod.conf /etc/nginx/nginx.conf
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled
-
-# sudo rm /etc/supervisor/conf.d/supervisor.conf
-sudo cp /var/wwwroot/%APP_NAME%/deployments/supervisor.prod.conf /etc/supervisor/conf.d/supervisor.conf
-
-# Disable apache2
-# sudo service apache2 stop && systemctl disable apache2
-
-# Restart
-sudo chown -R www-data:www-data /var/wwwroot/%APP_NAME%/storage
-sudo chown -R www-data:www-data /var/wwwroot/phpmyadmin
-systemctl restart php8.1-fpm
+cp /var/wwwroot/%APP_NAME%/deployments/site.prod.conf /etc/nginx/sites-available/site.conf &&\
+cp /var/wwwroot/%APP_NAME%/deployments/db-site.prod.conf /etc/nginx/sites-available/db.conf &&\
+cp /var/wwwroot/%APP_NAME%/deployments/default.prod.conf /etc/nginx/sites-available/default &&\
+cp /var/wwwroot/%APP_NAME%/deployments/nginx.prod.conf /etc/nginx/nginx.conf &&\
+sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled &&\
+sudo ln -s /etc/nginx/sites-available/db.conf /etc/nginx/sites-enabled &&\
+sudo cp /var/wwwroot/%APP_NAME%/deployments/supervisor.prod.conf /etc/supervisor/conf.d/supervisor.conf &&\
+sudo chown -R www-data:www-data /var/wwwroot/%APP_NAME%/storage &&\
+sudo chown -R www-data:www-data /var/wwwroot/phpmyadmin &&\
+systemctl restart php8.1-fpm &&\
 sudo service supervisor restart && service nginx restart
